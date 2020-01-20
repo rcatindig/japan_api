e5ffcfd29d01410184ec7a9b3a771e0508a10ab7
Reynard <rqcatindig@gmail.com>
2020-01-20 09:19:06 +0800

    fix menu and footer

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -3,14 +3,7 @@
     $route = (isset($r['as'])) ? $r['as'] : '';
 @endphp
 
-<li class="nav-item mT-30">
-    <a class="sidebar-link {{ Str::startsWith($route, ADMIN . '.dash') ? 'active' : '' }}" href="{{ route(ADMIN . '.dash') }}">
-        <span class="icon-holder">
-            <i class="c-blue-500 ti-home"></i>
-        </span>
-        <span class="title">Dashboard</span>
-    </a>
-</li>
+
 <li class="nav-item">
     <a class="sidebar-link {{ Str::startsWith($route, ADMIN . '.users') ? 'active' : '' }}" href="{{ route(ADMIN . '.users.index') }}">
         <span class="icon-holder">
@@ -23,7 +16,7 @@
 <li class="nav-item">
     <a class="sidebar-link {{ Str::startsWith($route, ADMIN . '.cities') ? 'active' : '' }}" href="{{ route(ADMIN . '.cities.index') }}">
         <span class="icon-holder">
-            <i class="c-brown-500 ti-user"></i>
+            <i class="c-brown-500 ti-map-alt"></i>
         </span>
         <span class="title">Cities</span>
     </a>
@@ -32,7 +25,7 @@
 <li class="nav-item">
     <a class="sidebar-link {{ Str::startsWith($route, ADMIN . '.tourist_spots') ? 'active' : '' }}" href="{{ route(ADMIN . '.tourist_spots.index') }}">
         <span class="icon-holder">
-            <i class="c-brown-500 ti-user"></i>
+            <i class="c-brown-500 ti-location-arrow"></i>
         </span>
         <span class="title">Tourist Spots</span>
     </a>
